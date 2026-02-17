0942acbb3f12e2e2fef6764c42b1420b0d429993
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-02-17 19:10:18 +0100

    Add Nullable to equals

--- problem4j-spring-web/src/main/java/io/github/problem4j/spring/web/parameter/Violation.java
+++ problem4j-spring-web/src/main/java/io/github/problem4j/spring/web/parameter/Violation.java
@@ -78,7 +78,7 @@ public class Violation implements Serializable {
   }
 
   @Override
-  public boolean equals(Object obj) {
+  public boolean equals(@Nullable Object obj) {
     if (this == obj) {
       return true;
     }
