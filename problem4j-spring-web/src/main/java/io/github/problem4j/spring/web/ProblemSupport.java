869fad5e07f0f7c9af295774764337485f1fbfaa
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-03-03 23:31:54 +0100

    Update deprecation notice
    Signed-off-by: Damian Malczewski <damian.m.malczewski@gmail.com>

--- problem4j-spring-web/src/main/java/io/github/problem4j/spring/web/ProblemSupport.java
+++ problem4j-spring-web/src/main/java/io/github/problem4j/spring/web/ProblemSupport.java
@@ -172,10 +172,10 @@ public final class ProblemSupport {
    * @param status the {@link HttpStatusCode} to resolve
    * @return the corresponding {@link ProblemStatus}, or {@link ProblemStatus#INTERNAL_SERVER_ERROR}
    *     if the status is {@code null} or invalid
-   * @deprecated Decided that {@link ProblemStatus} will be considered an internal utility of
-   *     Problem4J Core and will be made package-private in a future major release. The decision was
-   *     made to not confuse users of the library as each HTTP framework most likely has its own
-   *     enum for HTTP status codes.
+   * @deprecated {@link ProblemStatus} will be considered an internal utility of Problem4J Core and
+   *     will be made package-private in a future major release. The decision was made as each HTTP
+   *     framework most likely has its own enum for HTTP status codes, and there's no real value in
+   *     trying to maintain a separate, framework-agnostic enum that mirrors HTTP status codes.
    */
   @Deprecated(since = "1.2.7", forRemoval = true)
   public static ProblemStatus resolveStatus(HttpStatusCode status) {
