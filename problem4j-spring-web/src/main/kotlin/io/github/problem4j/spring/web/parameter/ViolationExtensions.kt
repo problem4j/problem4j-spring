910de3773a71bb4265ae0fea993c6e47eabfddf4
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-08-22 19:51:42 +0200

    Add violation dsl method

--- problem4j-spring-web/src/main/kotlin/io/github/problem4j/spring/web/parameter/ViolationExtensions.kt
+++ problem4j-spring-web/src/main/kotlin/io/github/problem4j/spring/web/parameter/ViolationExtensions.kt
@@ -1,6 +1,24 @@
 package io.github.problem4j.spring.web.parameter
 
 /**
+ * Creates a [Violation] for the given [field] and [error] message.
+ *
+ * Example:
+ * ```
+ * import io.github.problem4j.spring.web.parameter.violation
+ *
+ * val violation = violation("age", "must be positive")
+ * ```
+ *
+ * @param field name of the violating field; defaults to `null`
+ * @param error error message describing the violation; defaults to `null`
+ * @return a [Violation] with the given field and error
+ * @since 3.1.0
+ */
+public fun violation(field: String? = null, error: String? = null): Violation =
+    Violation(field, error)
+
+/**
  * Enables destructuring of [Violation] into its field component.
  *
  * Example:
